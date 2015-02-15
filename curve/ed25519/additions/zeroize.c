2bd1f5556fed92afb4efc2a95846a1c0f8332baf
Josue Ortega <josueortega@debian.org.gt>
2015-02-15 15:28:36 -0600

    Fixed prototype definition

--- curve/ed25519/additions/zeroize.c
+++ curve/ed25519/additions/zeroize.c
@@ -3,14 +3,14 @@
 void zeroize(unsigned char* b, size_t len)
 {
   size_t count = 0;
-  unsigned long retval = 0;
+  //unsigned long retval = 0;
   volatile unsigned char *p = b;
 
   for (count = 0; count < len; count++)
     p[count] = 0;
 }
 
-void zeroize_stack()
+void zeroize_stack(void)
 {
   unsigned char m[ZEROIZE_STACK_SIZE];
   zeroize(m, sizeof m);
